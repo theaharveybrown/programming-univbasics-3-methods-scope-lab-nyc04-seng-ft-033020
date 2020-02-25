f3b4cb327ec4ec605c3d73c93ca1051100665fac
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-02-25 18:40:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,10 +4,11 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!';
   puts status
 end
 
 def link
-  phrase = 
+  phrase = "It's Dangerous To Go Alone! Take This.";
+  
 end
